19aae1081067987a0163a33251329a518d30518d
PancreasMonster <ryanflood444@gmail.com>
2019-09-28 16:31:21 +0100

    Self Propel and UI

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public bool BillboardX = true;
+    public bool BillboardY = true;
+    public bool BillboardZ = true;
+    public float OffsetToCamera;
+    public Transform mbase;
+    Vector3 offset;
+    protected Vector3 localStartPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = mbase.position - transform.position;
+        localStartPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mbase.transform.position - offset;
+
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+                                                               Camera.main.transform.rotation * Vector3.up);
+        if (!BillboardX || !BillboardY || !BillboardZ)
+            transform.rotation = Quaternion.Euler(BillboardX ? transform.rotation.eulerAngles.x : 0f, BillboardY ? transform.rotation.eulerAngles.y : 0f, BillboardZ ? transform.rotation.eulerAngles.z : 0f);
+       // transform.localPosition = localStartPosition;
+        //transform.position = transform.position + transform.rotation * Vector3.forward * OffsetToCamera;
+    }
+}
--- Assets/Scripts/PropelSelf.cs
+++ Assets/Scripts/PropelSelf.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PropelSelf : MonoBehaviour
 {
+    public float force;
+    public Image bg, fill;
+    Rigidbody rb;
+    bool triggerDown = false;
+    float t, power;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("RightTrigger") > 0 && !triggerDown)
+        {
+            triggerDown = true;
+            bg.gameObject.SetActive(true);
+        }
+
+        if (triggerDown)
+        {
+            t += Time.deltaTime;
+            power = Mathf.Pow((Mathf.Sin(t)), 2);
+            fill.fillAmount = power;
+        }
+
+        if (Input.GetAxis("RightTrigger") == 0 && triggerDown)
+        {
+            rb.AddForce(transform.forward * force * power);
+            triggerDown = false;
+            t = 0;
+            power = 0;
+            fill.fillAmount = 0;
+            bg.gameObject.SetActive(false);
+        }
     }
 }
