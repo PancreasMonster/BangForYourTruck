becb795613d5f4c8139eac16a2a3346333ef0a67
PancreasMonster <ryanflood444@gmail.com>
2019-11-03 23:41:17 +0000

    Player Death State

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -37,6 +38,7 @@ public class Health : MonoBehaviour
                 Destroy(baseUI);
             Destroy(hpBarHolder);
             Destroy(this.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
