cb325c5338af11b2ebc319773e44e138a0dedda9
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-11-04 01:03:12 +0000

    Final touches for UI and terrain

---
+++ Assets/Nick's stuff/FindPlayerStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindPlayerStats : MonoBehaviour
+{
+    public GameObject player;
+
+    float playerHealth;
+    float playerMaxHealth;
+
+    Text resourceText;
+    Text resourceIncomeText;
+    Image hpBarFill;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        resourceText = transform.Find("Resources").GetComponent<Text>();
+        resourceIncomeText = transform.Find("Resources Income").GetComponent<Text>();
+        hpBarFill = GetComponentInChildren<PrototypeHexMapScript>().gameObject.GetComponent<Image>();
+        playerMaxHealth = player.GetComponent<Health>().maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = player.GetComponent<Health>().health;
+
+        resourceText.text = player.GetComponent<ResourceHolder>().resourceAmount.ToString();
+        //resourceIncomeText = player.GetComponent<Resources(New)>().resourceIncomeAmount.ToString();
+        hpBarFill.fillAmount = playerHealth / playerMaxHealth;
+
+        if (playerHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Resource Scripts/ResourceHolder.cs
+++ Assets/Scripts/Resource Scripts/ResourceHolder.cs
@@ -8,6 +8,7 @@ public class ResourceHolder : MonoBehaviour
 
     public int resourceAmount = 30;
     public Text resourceText;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class ResourceHolder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         resourceText.text = resourceAmount.ToString();
     }
 }
