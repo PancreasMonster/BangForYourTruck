e3efbc29c8e627caaaaf010721fe5380179167de
PancreasMonster <ryanflood444@gmail.com>
2019-11-05 23:11:46 +0000

    HP Bar Fixed

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -16,23 +16,26 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = health;
+        if (!mbase)
+        {
+            maxHealth = health;
 
-        GameObject hpBar = Instantiate(healthBarCanvas, new Vector3(transform.position.x, transform.position.y+1.5f, transform.position.z), Quaternion.identity);
-        hpBar.GetComponent<FaceCamera>().Cam1();
-        GameObject hpBar2 = Instantiate(healthBarCanvas, new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), Quaternion.identity);
-        hpBar2.GetComponent<FaceCamera>().Cam2();
-        hpBar.GetComponent<FaceCamera>().mbase = transform;
-        hpBar2.GetComponent<FaceCamera>().mbase = transform;
-        hpBarHolder = hpBar;
-        hpBarHolder2 = hpBar2;
-        hpBarFill = hpBar.GetComponentInChildren<PrototypeHexMapScript>().gameObject.GetComponent<Image>();
+            GameObject hpBar = Instantiate(healthBarCanvas, new Vector3(transform.position.x, transform.position.y + 15f, transform.position.z), Quaternion.identity);
+            hpBar.GetComponent<FaceCamera>().Cam1();
+            GameObject hpBar2 = Instantiate(healthBarCanvas, new Vector3(transform.position.x, transform.position.y + 15f, transform.position.z), Quaternion.identity);
+            hpBar2.GetComponent<FaceCamera>().Cam2();
+            hpBar.GetComponent<FaceCamera>().mbase = transform;
+            hpBar2.GetComponent<FaceCamera>().mbase = transform;
+            hpBarHolder = hpBar;
+            hpBarHolder2 = hpBar2;
+            hpBarFill = hpBar.GetComponentInChildren<PrototypeHexMapScript>().gameObject.GetComponent<Image>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        hpBarFill.fillAmount = health / maxHealth;
+        //hpBarFill.fillAmount = health / maxHealth;
         if (health <= 0)
         {
             if (mbase)
