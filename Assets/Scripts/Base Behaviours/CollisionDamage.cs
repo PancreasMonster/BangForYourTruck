76f5dcef7fb47e1258bd1b9c0891b5840ef7dd8e
PancreasMonster <ryanflood444@gmail.com>
2019-11-12 20:14:27 +0000

    Collision Damage

---
+++ Assets/Scripts/Base Behaviours/CollisionDamage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour
+{
+
+
+    public float oldVelocity, minimumForce;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        oldVelocity = rb.velocity.sqrMagnitude;
+        minimumForce *= minimumForce;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        oldVelocity = rb.velocity.sqrMagnitude;
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.transform.tag == "Player")
+        {
+            float damage = Mathf.RoundToInt(Mathf.Min(20, oldVelocity / 100));
+            coll.transform.GetComponent<Health>().health -= damage;
+        }
+    }
+}
