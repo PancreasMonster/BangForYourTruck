6ed2f27610106c031f9592cfd0cd3f22c9237c26
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-11-17 17:12:00 +0000

    Player respawner added

---
+++ Assets/ReturnPlayerToStartPos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnPlayerToStartPos : MonoBehaviour
+{
+    public GameObject player1;
+    Vector3 player1StartPos;
+
+    public GameObject player2;
+    Vector3 player2StartPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1StartPos = new Vector3(player1.transform.position.x, player1.transform.position.y, player1.transform.position.z);
+        player2StartPos = new Vector3(player2.transform.position.x, player2.transform.position.y, player2.transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player1)
+        {
+            player1.transform.position = player1StartPos;
+
+        }
+
+        if (other.gameObject == player2)
+        {
+            player2.transform.position = player2StartPos;
+
+        }
+    }
+}
