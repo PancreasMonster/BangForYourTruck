0a5ed313287d6165e0e00d3f0bd6bba0f42d6c49
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-11-19 22:17:27 +0000

    Buildmode prototype inputs done

--- Assets/PrototypeBuildMode.cs
+++ Assets/PrototypeBuildMode.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class PrototypeBuildMode : MonoBehaviour
 {
+    public float moveSpeed;
+    Rigidbody rb;
+
+    public GameObject capsuleChild;
+    public float yRotationSpeed = 5f;
+    public float xRotationSpeed = .5f;
+    public float maxXRot;
+    public float minXRot;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        if (Input.GetAxis("Mouse X") < 0)
+            transform.Rotate((Vector3.up) * -yRotationSpeed);
+        if (Input.GetAxis("Mouse X") > 0)
+            transform.Rotate((Vector3.up) * yRotationSpeed);
+
+        if (Input.GetKey("a"))
+        {
+            Debug.Log("a");
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Debug.Log("d");
+            transform.Translate(Vector3.left * -moveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            Debug.Log("w");
+            if (transform.rotation.x <= maxXRot)
+            {
+                Debug.Log("looking down");
+
+
+                capsuleChild.transform.Rotate(transform.rotation.x + xRotationSpeed, transform.rotation.y, transform.rotation.z, Space.World);
+            }
+        }
+
+        if (Input.GetKey("s"))
+        {
+            if (transform.rotation.x >= minXRot)
+            {
+                Debug.Log("looking up");
+
+                capsuleChild.transform.Rotate(transform.rotation.x - xRotationSpeed, transform.rotation.y, transform.rotation.z, Space.World);
+            }
+        }
     }
 }
