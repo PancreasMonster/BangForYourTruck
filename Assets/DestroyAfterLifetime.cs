e4ce9ba9a68871118e1f2f0d20048f175966c011
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-11-19 21:28:52 +0000

    Starting Buildmode Prototype

---
+++ Assets/DestroyAfterLifetime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterLifetime : MonoBehaviour
+{
+    public float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyThisGameObject", lifetime);
+    }
+
+    
+
+    private void DestroyThisGameObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
