dec747a745b7da1f676997390239f0738da97d0e
PancreasMonster <ryanflood444@gmail.com>
2019-11-24 14:59:00 +0000

    Work On Build Mode

---
+++ Assets/Scripts/Base Behaviours/Build Mode/BuildModeMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildModeMovement : MonoBehaviour
+{
+
+    public float suspensionForce;
+    public float suspensionLenght;
+    public float dampingForce;
+    public List<Transform> hoverPoints = new List<Transform>();
+    RaycastHit flHit, frHit, blHit, brHit;
+    Rigidbody rb;
+    public LayerMask layer;
+
+    private float springVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HoverForce();
+    }
+
+    void HoverForce ()
+    {
+        RaycastHit hit;
+        foreach (Transform t in hoverPoints)
+        {
+            if (Physics.Raycast(t.position, -transform.up, out hit, suspensionLenght, layer))
+            {
+                rb.AddForceAtPosition(hit.normal.normalized * suspensionForce * (1.0f - (hit.distance / suspensionLenght)), t.position);
+                Debug.DrawLine(t.position, hit.point, Color.blue);
+            }
+            else
+            {
+                if(transform.position.y > t.transform.position.y)
+                {
+                    rb.AddForceAtPosition(t.transform.up * suspensionForce, t.transform.position); 
+                } else
+                {
+                    rb.AddForceAtPosition(t.transform.up * -suspensionForce, t.transform.position);
+                }
+            } 
+
+
+        }
+        
+    }
+}
