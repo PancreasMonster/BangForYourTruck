7d6a0ed748490d240a43ae3d5eb98cb6a972149d
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-11-27 21:31:44 +0000

    Added gates and blue materials

--- Assets/Scripts/PowerUp Scripts/EnergyRefill.cs
+++ Assets/Scripts/PowerUp Scripts/EnergyRefill.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class EnergyRefill : MonoBehaviour
 {
     public float powerRegenIncrease = .5f;
+    public bool respawn;
+    public float respawnTimer;
+
+    Transform child;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        child = gameObject.transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -25,7 +29,24 @@ public class EnergyRefill : MonoBehaviour
             PowerHolder ph = other.GetComponent<PowerHolder>();
             ph.powerAmount = ph.maxPower;
             ph.powerRegen += powerRegenIncrease;
+        }
+        if (respawn)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            child.gameObject.SetActive(false);
+
+            Invoke("Respawn", respawnTimer);
+        }
+        else
+        {
             Destroy(this.gameObject);
         }
+    }
+
+    void Respawn()
+    {
+        GetComponent<BoxCollider>().enabled = true;
+        child.gameObject.SetActive(true);
+
     }
 }
