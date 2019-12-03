cac24f7a27535c7f25012bfab5ce183236bc47db
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-12-03 00:22:54 +0000

    Ui and particle additions

---
+++ Assets/ItemPickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public ParticleSystem ps;
+    //ParticleSystem.EmissionModule emissionModule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = transform.Find("Item Pickup Particle").GetComponent<ParticleSystem>();
+        //emissionModule = ps.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Pickup")
+        {
+            Debug.Log("pickup");
+            ps.Play();
+
+        }
+    }
+}
