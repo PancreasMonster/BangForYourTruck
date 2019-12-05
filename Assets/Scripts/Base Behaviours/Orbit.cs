cd8219bd891a65215fe28bc223cc6ca04ef445d7
PancreasMonster <ryanflood444@gmail.com>
2019-12-05 14:07:01 +0000

    Camera Changes

--- Assets/Scripts/Base Behaviours/Orbit.cs
+++ Assets/Scripts/Base Behaviours/Orbit.cs
@@ -15,6 +15,7 @@ public class Orbit : MonoBehaviour
     private float timer;
     public Vector3 offset = new Vector3(0, 10, -26);
     private Vector3 origPos;
+    private Vector3 jumpOffset;
     private bool lockedBehind = true;
     private bool disorient;
     public FlipOver fo;
@@ -22,6 +23,8 @@ public class Orbit : MonoBehaviour
     public float animSpeed;
     private WheelCollider[] wheels;
     public bool death;
+    public float lookOffsetY = 3;
+    public float yLookAmount;
 
     void Start()
     {
@@ -75,28 +78,28 @@ public class Orbit : MonoBehaviour
                 {
                     if (!disorient)
                     {
-                        offset = transform.position - player.transform.position;
+                        jumpOffset = transform.position - player.transform.position;
                         disorient = true;
                     }
 
-                    transform.position = player.position + offset;
-                    offset = Quaternion.AngleAxis(Input.GetAxisRaw("RHorizontal" + playerNum.ToString()) * turnSpeed * Time.deltaTime, Vector3.up) * offset;
-                    transform.LookAt(player.position);
+                    transform.position = player.position + jumpOffset;
+                    jumpOffset = Quaternion.AngleAxis(Input.GetAxisRaw("RHorizontal" + playerNum.ToString()) * turnSpeed * Time.deltaTime, Vector3.up) * jumpOffset;
+                    transform.LookAt(new Vector3(player.position.x, player.position.y + lookOffsetY, player.position.z));
                 }
                 else
                 {
-                    transform.position = new Vector3(player.TransformPoint(origPos).x, player.position.y + 10, player.TransformPoint(origPos).z);
+                    transform.position = new Vector3(player.TransformPoint(origPos).x, player.position.y + offset.y + (Input.GetAxisRaw("RVertical" + playerNum.ToString()) * yLookAmount), player.TransformPoint(origPos).z);
                     disorient = false;
                 }
 
-                transform.LookAt(player.position);
+                transform.LookAt(new Vector3(player.position.x, player.position.y + lookOffsetY, player.position.z));
             }
 
             if (!lockedBehind)
             {
                 transform.position = player.position + offset;
                 offset = Quaternion.AngleAxis(Input.GetAxisRaw("RHorizontal" + playerNum.ToString()) * turnSpeed * Time.deltaTime, Vector3.up) * offset;
-                transform.LookAt(player.position);
+                transform.LookAt(new Vector3(player.position.x, player.position.y + lookOffsetY, player.position.z));
             }
         }
         else
