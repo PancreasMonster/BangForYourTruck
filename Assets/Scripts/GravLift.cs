2312bb27d4e8157086cb202623b31e4b8e4e3c96
PancreasMonster <ryanflood444@gmail.com>
2019-12-08 23:24:27 +0000

    Gravity Lift

---
+++ Assets/Scripts/GravLift.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravLift : MonoBehaviour
+{
+    public float gravForce = 10000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+           other.transform.GetComponent<Rigidbody>().AddForce(transform.forward * gravForce);
+        }
+    }
+}
