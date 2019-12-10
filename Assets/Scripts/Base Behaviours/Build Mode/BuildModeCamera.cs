762bb6e74f8c85dbc6399f205600a44a3c62ed41
PancreasMonster <ryanflood444@gmail.com>
2019-12-10 16:48:27 +0000

    Another small build mode QoL change

--- Assets/Scripts/Base Behaviours/Build Mode/BuildModeCamera.cs
+++ Assets/Scripts/Base Behaviours/Build Mode/BuildModeCamera.cs
@@ -31,6 +31,7 @@ public class BuildModeCamera : MonoBehaviour
     public GameObject buildModeDpad;
     bool buildMode;
     public Vector3 origOffset_;
+    public float targetYOffset = 30;
 
     // Start is called before the first frame update
     void Start()
@@ -124,7 +125,7 @@ public class BuildModeCamera : MonoBehaviour
         offset = new Vector3(offset.x, verticalHeight, offset.z);
         transform.position = player.position + offset;
         offset = Quaternion.AngleAxis(Input.GetAxisRaw("RHorizontal" + playerNum.ToString()) * turnSpeed * Time.deltaTime, Vector3.up) * offset;       
-        transform.LookAt(new Vector3(target.position.x, target.position.y - 30, target.position.z));
+        transform.LookAt(new Vector3(target.position.x, target.position.y - targetYOffset, target.position.z));
         mainCam.fieldOfView = mainCamFoVBaseValue + (mainCamFoVScaleValue * (1.0f - ((verticalHeight-minYOffset)/minMaxOffset)));
         verticalHeight = Mathf.Clamp(verticalHeight, minYOffset, maxYOffset);
         if(verticalHeight >= minYOffset && verticalHeight <= maxYOffset)
