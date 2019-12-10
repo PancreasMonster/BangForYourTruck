0caf7ba63226e22b76d23cdc35d48795de382772
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2019-12-10 19:03:58 +0000

    Drag changer script

---
+++ Assets/DragChangeOnLanding.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragChangeOnLanding : MonoBehaviour
+{
+    public float newDrag;
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 14)
+        {
+            GetComponent<Rigidbody>().drag = newDrag;
+
+        }
+    }
+}
