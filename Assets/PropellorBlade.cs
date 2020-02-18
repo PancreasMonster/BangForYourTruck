220decd83d38fc47514453fdf15c02b4b89473d9
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-02-18 00:08:53 +0000

    Propellor blades ready

---
+++ Assets/PropellorBlade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellorBlade : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+    float velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.transform.parent.gameObject.transform.parent.gameObject;
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        velocity = rb.velocity.magnitude;
+        //newRotation = (transform.rotation.x, transform.rotation.y, transform.rotation.z);
+        transform.Rotate(Vector3.left * velocity * 10f * Time.deltaTime);
+    }
+}
