76f7f51fc51864d4262d756ca876d8321f980d8b
PancreasMonster <ryanflood444@gmail.com>
2020-02-25 22:15:38 +0000

    Call Stuff

---
+++ Assets/Scripts/Tag Gamemode/PurchaseGate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseGate : MonoBehaviour
+{
+    public int price;
+    public int ammoType;
+    public int ammoAddition;
+    public int teamGateNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.transform.tag == "Player")
+        {
+            if (col.GetComponent<Health>().teamNum == teamGateNum)
+            {
+                PlayerBank PB = col.GetComponent<PlayerBank>();
+                if (PB.tagsInBank >= price)
+                {
+                    col.GetComponent<BuildModeFire>().ammo[ammoType] += ammoAddition;
+                    PB.tagsInBank -= price;
+                }
+            }
+        }
+    }
+}
