3d4e82d7834da9aa6d95ae8f488e10a77f106d3b
PancreasMonster <ryanflood444@gmail.com>
2020-02-25 12:11:28 +0000

    More Tag Implementation

--- Assets/Scripts/Tag Gamemode/TagCollectionManager.cs
+++ Assets/Scripts/Tag Gamemode/TagCollectionManager.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TagCollectionManager : MonoBehaviour
 {
-    public Text text;
-    public Image image;
-    public int blueTeamTokens, redTeamTokens;
+    public Text blueText, redText, winText;
+    public Image redImage, blueImage;
+    public int blueTeamTokens, redTeamTokens, gameWinningAmount = 30;
+    bool gameWon;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +17,38 @@ public class TagCollectionManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!gameWon)
+        {
+            blueText.text = "Blue Team: " + blueTeamTokens.ToString() + "/" + gameWinningAmount.ToString();
+            redText.text = "Red Team: " + redTeamTokens.ToString() + "/" + gameWinningAmount.ToString();
+            redImage.fillAmount = redTeamTokens / gameWinningAmount;
+            blueImage.fillAmount = blueTeamTokens / gameWinningAmount;
+        }
+
+        if (blueTeamTokens > gameWinningAmount && !gameWon)
+        {
+            gameWon = true;
+            blueText.text = "";
+            redText.text = "";
+            winText.text = "Blue Team Has Won!";
+            StartCoroutine(sceneReload());
+        }
+        else if (redTeamTokens > gameWinningAmount && !gameWon)
+        {
+            gameWon = true;
+            blueText.text = "";
+            redText.text = "";
+            winText.text = "Red Team Has Won!";
+            StartCoroutine(sceneReload());
+        }
+    }
+
+    IEnumerator sceneReload()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
+
