698c2db4acdb1c08f07ba309f9b7f9109781bbc7
PancreasMonster <ryanflood444@gmail.com>
2020-02-27 21:43:54 +0000

    Updated CreateLevel Script

--- Assets/Scripts/CreateLevel.cs
+++ Assets/Scripts/CreateLevel.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CreateLevel : MonoBehaviour
 {
-    public List<Transform> players = new List<Transform>();
     public List<GameObject> levels = new List<GameObject>();
 
     // Start is called before the first frame update
@@ -21,10 +20,6 @@ public class CreateLevel : MonoBehaviour
 
     public void activateLevel (int levelIndex)
     {
-        levels[levelIndex].SetActive(true);
-        for(int i = 0; i < players.Count; i++)
-        {
-            players[i].position = levels[levelIndex].transform.Find("PlayerSpawns").transform.Find("Player" + i.ToString() + "SpawnPoint").position;
-        }
+        levels[levelIndex].SetActive(true);       
     }
 }
