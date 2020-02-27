26c7093d8adb1ac70f17e8dc6e40b12234684ba8
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-02-27 21:44:40 +0000

    Spawn Zones activated

--- Assets/SpawnPoint.cs
+++ Assets/SpawnPoint.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    public int playerNum;
+    GameObject[] players;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<MeshRenderer>().enabled = false;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject t in players)
+        {
+
+            if (t.GetComponent<Health>().playerNum == playerNum)
+            {
+                t.transform.position = transform.position;
+                t.transform.rotation = transform.rotation;
+            }
+        }
     }
 }
