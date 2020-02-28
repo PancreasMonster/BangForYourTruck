ad71217769ca3be94f417a6aa3a05016a0e238c7
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-02-28 02:30:00 +0000

    Main menu UI implementation and Scene loading fix

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -7,7 +7,7 @@ public class LevelCreator : MonoBehaviour
 
     
 {
-
+    public bool mainMenu;
     public bool singlePlayer;
     public int levelToActivate;
     // Start is called before the first frame update
@@ -19,15 +19,21 @@ public class LevelCreator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape) && !mainMenu)
         {
             SceneManager.LoadScene("0_MainMenu", LoadSceneMode.Single);
+            Destroy(this.gameObject);
         }
     }
 
-    public void ToggleSinglePlayer()
+    public void SetSinglePlayer()
     {
-        singlePlayer = !singlePlayer;
+        singlePlayer = true;
+    }
+
+    public void SetMultiPlayer()
+    {
+        singlePlayer = false;            
     }
 
     public void SetLevelIndex (int i)
@@ -36,8 +42,14 @@ public class LevelCreator : MonoBehaviour
         LoadScene();
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
     public void LoadScene()
     {
+        mainMenu = false;
         if(singlePlayer)
         {
             SceneManager.LoadScene("SinglePlayerScene", LoadSceneMode.Single);
