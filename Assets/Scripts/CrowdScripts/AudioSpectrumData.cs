1d94f31c61544880d7b27db6638f76c10b9e4bbd
PancreasMonster <ryanflood444@gmail.com>
2020-03-03 01:03:25 +0000

    AudioVisualiserStadium

---
+++ Assets/Scripts/CrowdScripts/AudioSpectrumData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSpectrumData : MonoBehaviour
+{
+    
+    //public GameObject[][] crowdSections = new GameObject[10][];
+    AudioSource aud;
+    public float[] audSamples = new float[512];
+    public Color col1, col2;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        aud = GameObject.Find("MainGameMusic").GetComponent<AudioSource>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrum();
+    }
+
+    void GetSpectrum()
+    {
+        aud.GetSpectrumData(audSamples, 0, FFTWindow.Blackman); // collects samples from the audio source
+        for(int i = 0; i < 256; i++)
+        {
+            audSamples[i] *= 1500;
+        }        
+    }
+
+    
+}
---
+++ Assets/Scripts/CrowdScripts/CrowdSection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CrowdSection : MonoBehaviour
+{
+    public List<GameObject> crowdSections = new List<GameObject>();
+
+    public AudioSpectrumData ASD;
+    float maxScale = 1;
+    public int bandNum;
+    bool start = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //ASD = GameObject.FindGameObjectWithTag("StadiumSeparatedCrowd").GetComponent<AudioSpectrumData>();
+         foreach (Transform child in transform)
+         {
+             crowdSections.Add(child.transform.gameObject);
+         }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < crowdSections.Count; i++)
+        {
+            if (((float)(i + 1 / crowdSections.Count)) < ((float)ASD.audSamples[bandNum]))
+            {
+                
+                crowdSections[i].GetComponent<Renderer>().material.SetColor("Color_EED75798", ASD.col2);
+               
+    }
+            else
+            {
+               
+                crowdSections[i].GetComponent<Renderer>().material.SetColor("Color_EED75798", ASD.col1);
+
+            }
+        }
+    }
+
+    
+
+    public void AssignBandNum(int num)
+    {
+        bandNum = num;
+    }
+}
