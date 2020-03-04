1a938129c5b1f9552a20efd3ca1ac1085244dcaf
PancreasMonster <ryanflood444@gmail.com>
2020-03-04 13:54:50 +0000

    New Suspension

---
+++ Assets/Scripts/Base Behaviours/HoverPartScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverPartScript : MonoBehaviour
+{
+    public Transform wheelTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(wheelTarget);
+    }
+}
