6c0d85db753d04fed1128c4b1969d81ef9703b16
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-03-14 17:32:55 +0000

    Shotgun Added, stilla couple bugs present

---
+++ Assets/ShotgunBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunBullet : MonoBehaviour
+{
+    public int teamNum;
+    public GameObject damageSource; //the source of this GameObject ie. the player that instantiated the 'bullet' 
+    public float damageToDeal;
+    public float maxDamage;
+    public float bulletLifetime;
+    //public float growthOverTime;
+    public float damageFallOffOvertTime;
+
+    public bool player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<DestroyAfterLifetime>().lifetime = bulletLifetime;
+        damageToDeal = maxDamage;
+
+        if (player)
+            damageSource = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bulletLifetime -= Time.deltaTime;
+        if (bulletLifetime <= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+
+        damageToDeal = damageToDeal - (Time.deltaTime * damageFallOffOvertTime);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<Health>() != null && other.transform.GetComponent<Health>().teamNum != teamNum)
+        {
+            other.transform.GetComponent<Health>().TakeDamage(null, damageSource, damageToDeal, Vector3.zero);
+        }
+    }
+}
---
+++ Assets/ShotgunWeapon.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunWeapon : MonoBehaviour
+{
+    public GameObject model;
+    public float force;
+    Transform ShotgunWeaponFiringPoint;
+    Transform ShotgunWeaponFiringPoint2;
+    public GameObject weaponProjectile;
+    PowerHolder ph;
+    public PowerCosts pc;
+    ParticleSystem particles1;
+    ParticleSystem particles2;
+
+    bool rightFiredLast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = GameObject.Find("PowerCost").GetComponent<PowerCosts>();
+        model.SetActive(true);
+        ShotgunWeaponFiringPoint = transform.Find("Trails and firing points").transform.Find("ShotgunWeaponFiringPoint").transform;
+        ShotgunWeaponFiringPoint2 = transform.Find("Trails and firing points").transform.Find("ShotgunWeaponFiringPoint2").transform;
+        particles1 = ShotgunWeaponFiringPoint.gameObject.GetComponent<ParticleSystem>();
+        particles2 = ShotgunWeaponFiringPoint2.gameObject.GetComponent<ParticleSystem>();
+        ph = GetComponent<PowerHolder>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("PadRB" + GetComponent<Health>().playerNum.ToString()))
+        {
+            if (!rightFiredLast)
+            {
+
+                FireBullet();
+            }
+            else
+            {
+                FireBullet2();
+
+            }
+        }
+    }
+
+    void FireBullet()
+    {
+        if (ph.powerAmount >= pc.powerCosts[5])
+        {
+
+            GameObject Bullet = Instantiate(weaponProjectile, ShotgunWeaponFiringPoint.position, ShotgunWeaponFiringPoint.transform.rotation);
+
+            Bullet.GetComponent<ShotgunBullet>().teamNum = GetComponent<Health>().teamNum;
+
+            Bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+
+            Bullet.GetComponent<ShotgunBullet>().damageSource = this.gameObject;
+
+            particles1.Play();
+
+            ph.losePower(pc.powerCosts[2]);
+
+            rightFiredLast = true;
+            Debug.Log("right fired");
+        }
+    }
+
+    void FireBullet2()
+    {
+        if (ph.powerAmount >= pc.powerCosts[5])
+        {
+
+            GameObject Bullet = Instantiate(weaponProjectile, ShotgunWeaponFiringPoint2.position, ShotgunWeaponFiringPoint2.transform.rotation);
+
+            Bullet.GetComponent<ShotgunBullet>().teamNum = GetComponent<Health>().teamNum;
+
+            Bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+
+            Bullet.GetComponent<ShotgunBullet>().damageSource = this.gameObject;
+
+            particles2.Play();
+
+            ph.losePower(pc.powerCosts[2]);
+
+            rightFiredLast = false;
+            Debug.Log("left fired");
+
+        }
+    }
+}
