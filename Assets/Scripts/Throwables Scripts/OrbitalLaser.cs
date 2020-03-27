ae0e6df449298761c49c444706a5bfd1f048e17a
PancreasMonster <ryanflood444@gmail.com>
2020-03-27 16:45:20 +0000

    Orbital Laser

---
+++ Assets/Scripts/Throwables Scripts/OrbitalLaser.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalLaser : MonoBehaviour
+{
+   
+    public float laserSpeed;
+
+    public float laserLifeTime;
+
+    public float laserStartTime;
+
+    bool laserActivate;
+
+    Vector3 nextWaypoint;
+
+    Rigidbody rb;
+
+    public float arriveDistance;
+
+    public List<Vector3> laserWaypoints = new List<Vector3>();
+
+    public int next = 0;
+    public bool looped = true;
+
+    public Transform followPlayer;
+
+    public int teamNum;
+
+    public void OnDrawGizmos()
+    {
+        int count = looped ? (transform.childCount + 1) : transform.childCount;
+        Gizmos.color = Color.cyan;
+        for (int i = 1; i < count; i++)
+        {
+            Transform prev = transform.GetChild(i - 1);
+            Transform next = transform.GetChild(i % transform.childCount);
+            Gizmos.DrawLine(prev.transform.position, next.transform.position);
+            Gizmos.DrawSphere(prev.position, 1);
+            Gizmos.DrawSphere(next.position, 1);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, laserLifeTime);
+        StartCoroutine(laserActivation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (laserActivate)
+        {
+             nextWaypoint = NextWaypoint();
+
+             Vector3 dir = nextWaypoint - transform.position;
+             dir.Normalize();
+
+             transform.Translate(dir * laserSpeed * Time.deltaTime);
+
+             if (Vector3.Distance(transform.position, nextWaypoint) < arriveDistance)
+             {
+                 AdvanceToNext();
+             } 
+
+            /*Vector3 dir = followPlayer.position - transform.position;
+            dir.Normalize();
+            transform.Translate(dir * laserSpeed * Time.deltaTime);*/
+
+        }
+    }
+
+    public Vector3 NextWaypoint()
+    {
+        return laserWaypoints[next];
+    }
+
+    public void AdvanceToNext()
+    {
+        if (looped)
+        {
+            next = (next + 1) % laserWaypoints.Count;
+        }
+        else
+        {
+            if (next != laserWaypoints.Count - 1)
+            {
+                next++;
+            }
+        }
+    }
+
+    IEnumerator laserActivation ()
+    {
+        yield return new WaitForSeconds(laserStartTime);
+        laserActivate = true;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.GetComponent<Health>() != null && col.gameObject != this.gameObject)
+        {
+            if (col.gameObject.GetComponent<Health>().teamNum != teamNum)
+            {
+                col.gameObject.GetComponent<Health>().TakeDamage("Lasered", followPlayer.gameObject, 100, Vector3.zero);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Throwables Scripts/OrbitalLaserSpawn.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalLaserSpawn : MonoBehaviour
+{
+    bool laserSpawned = false;
+
+    public float laserRechargeTime;
+
+    public GameObject orbitalLaser, currentOrbitalLaser;
+
+    public float zPosBehind;
+
+    public float laserHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("DPADVertical" + GetComponent<Health>().playerNum.ToString()) > 0 && !laserSpawned)
+        {
+            laserSpawned = true;
+            GameObject clone = Instantiate(orbitalLaser, new Vector3(transform.position.x, transform.position.y + laserHeight, transform.position.z + zPosBehind), orbitalLaser.transform.rotation);
+            currentOrbitalLaser = clone;
+            currentOrbitalLaser.GetComponent<OrbitalLaser>().laserWaypoints.Add(currentOrbitalLaser.transform.position);
+            currentOrbitalLaser.GetComponent<OrbitalLaser>().followPlayer = transform;
+            currentOrbitalLaser.GetComponent<OrbitalLaser>().teamNum = GetComponent<Health>().teamNum;
+            StartCoroutine(CreateLaserWaypoint());
+            StartCoroutine(RechargeLaser());
+        }
+
+
+    }
+
+    IEnumerator CreateLaserWaypoint()
+    {
+        yield return new WaitForSeconds(1);
+        if (currentOrbitalLaser)
+        {
+            currentOrbitalLaser.GetComponent<OrbitalLaser>().laserWaypoints.Add(new Vector3(transform.position.x, transform.position.y + laserHeight, transform.position.z));
+            StartCoroutine(CreateLaserWaypoint());
+        }
+    }
+
+    IEnumerator RechargeLaser()
+    {
+        yield return new WaitForSeconds(laserRechargeTime);
+        laserSpawned = false;
+    }
+}
