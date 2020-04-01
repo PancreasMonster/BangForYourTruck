5b060d675d11c0c688713dcf90a9a5ad9e530fec
PancreasMonster <ryanflood444@gmail.com>
2020-04-01 22:10:41 +0100

    PlayerInput

---
+++ Assets/Scripts/PlayerInput/CubeMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CubeMovement : MonoBehaviour
+{
+    public Vector2 i_movement;
+    public float moveSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * moveSpeed * Time.deltaTime;
+       
+        transform.Translate(movement);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        i_movement = value.Get<Vector2>();
+    }
+
+    private void OnDeviceLost()
+    {
+        i_movement = Vector2.zero;
+    }
+}
