2fee47fb4e12cfdc84442beb02619e1e32814add
PancreasMonster <ryanflood444@gmail.com>
2020-04-01 16:27:55 +0100

    Enter Into Arena Done

---
+++ Assets/Scripts/EntranceScripts/EntranceChangeLayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceChangeLayer : MonoBehaviour
+{
+    public int layerNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.transform.tag == "Player")
+        {
+            coll.gameObject.layer = layerNum;
+            foreach(Transform t in coll.transform)
+            {
+                t.gameObject.layer = layerNum;
+            }
+        }
+    }
+}
