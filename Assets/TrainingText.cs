38a1eea99ae8de3f45e817fcbfc4efc78fc8a08e
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-04-01 16:50:27 +0100

    Training text implemented

---
+++ Assets/TrainingText.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingText : MonoBehaviour
+{
+    public int trainingStage;
+    bool isTraining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.Find("PersistentSceneLoader").GetComponent<LevelCreator>().singlePlayer == true)
+        {
+            isTraining = true;
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            isTraining = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button7) && isTraining)
+        {
+            if (trainingStage == 1 || trainingStage ==2)
+            {
+                Debug.Log("Pressed start");
+                GoToNextTraining();
+            }           
+        }
+    }
+
+    
+
+    public void GoToNextTraining()
+    {
+        trainingStage++;
+        Debug.Log("Going to next training");
+
+        if (trainingStage == 1)
+        {
+            GoToJumpDriftBoostLockOnText();
+        }
+
+        if (trainingStage == 2)
+        {
+            GoToFightingText();
+        }
+
+        if (trainingStage == 3)
+        {
+            GoToCollectingTagsText();
+        }
+
+        if (trainingStage == 4)
+        {
+            GoToBuyingThrowablesText();
+        }
+
+        if (trainingStage == 5)
+        {
+            GoToUsingThrowablesText();
+        }
+    }
+
+    public void GoToJumpDriftBoostLockOnText()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    public void GoToFightingText()
+    {
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(true);
+    }
+
+    public void GoToCollectingTagsText()
+    {
+        transform.GetChild(2).gameObject.SetActive(false);
+        transform.GetChild(3).gameObject.SetActive(true);
+    }
+
+    public void GoToBuyingThrowablesText()
+    {
+        transform.GetChild(3).gameObject.SetActive(false);
+        transform.GetChild(4).gameObject.SetActive(true);
+    }
+
+    public void GoToUsingThrowablesText()
+    {
+        transform.GetChild(4).gameObject.SetActive(false);
+        transform.GetChild(5).gameObject.SetActive(true);
+    }
+}
---
+++ Assets/TrainingTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingTrigger : MonoBehaviour
+{
+    TrainingText trainingManager;
+
+    public int trainingStage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trainingManager = GameObject.Find("Training Canvas").GetComponent<TrainingText>();
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (trainingManager.trainingStage == trainingStage)
+        {
+            trainingManager.GoToNextTraining();
+            gameObject.SetActive(false);
+        }       
+    }
+}
