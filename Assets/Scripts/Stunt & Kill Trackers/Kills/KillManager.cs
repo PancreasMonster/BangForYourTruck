9161cc191ce73f1123535ef46f045b405533ae1e
PancreasMonster <ryanflood444@gmail.com>
2020-04-03 14:03:21 +0100

    Input Gods, have Mercy on Me

--- Assets/Scripts/Stunt & Kill Trackers/Kills/KillManager.cs
+++ Assets/Scripts/Stunt & Kill Trackers/Kills/KillManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KillManager : MonoBehaviour
 {
@@ -24,15 +25,57 @@ public class KillManager : MonoBehaviour
 
     public KillFeed kf;
 
+    public List<PlayerInput> PI = new List<PlayerInput>();
+
+    public PlayerInputManager pim;
+
     // Start is called before the first frame update
     void Start()
     {
         FMODUnity.RuntimeManager.PlayOneShot(startSound, players[0].transform.position);
+
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
+        
+
+        int conTrollerCount = 0;
+
+    string[] names = Input.GetJoystickNames();
+        for (int x = 0; x < names.Length; x++)
+        {
+            print(names[x].Length);
+            if (names[x].Length == 19)
+            {
+                print("PS4 CONTROLLER IS CONNECTED");
+                conTrollerCount++;
+            }
+            if (names[x].Length == 33)
+            {
+                print("XBOX ONE CONTROLLER IS CONNECTED");
+                //set a controller bool to true
+                conTrollerCount++;
+
+            }
+        }
+
+
+        Debug.Log(conTrollerCount);
+    
+        for (int i = 0; i < PI.Count; i++)
+        {
+            if((i+1) <= conTrollerCount)
+            {
+                PI[i].enabled = true;
+            } else
+            {
+                PI[i].enabled = false;
+            }
+        }
 
     }
 
