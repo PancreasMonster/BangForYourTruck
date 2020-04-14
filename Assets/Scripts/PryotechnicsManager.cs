0d688fe2c5092c5744f59a0207ae6e74d66a1260
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-04-15 00:01:36 +0100

    Blimp anim fixed, stadium pyrotechnics implemented

---
+++ Assets/Scripts/PryotechnicsManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PryotechnicsManager : MonoBehaviour
+{
+    public ParticleSystem[] redTeamParticles;
+    public ParticleSystem[] blueTeamParticles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            RedTeamParticlesFire();
+        }
+
+        if (Input.GetKeyDown("y"))
+        {
+            BlueTeamParticlesFire();
+        }
+    }
+
+    public void RedTeamParticlesFire()
+    {
+        for(int i = 0; i < redTeamParticles.Length; i++)
+        {
+            redTeamParticles[i].Play();
+        }
+    }
+
+    public void BlueTeamParticlesFire()
+    {
+        for (int i = 0; i < blueTeamParticles.Length; i++)
+        {
+            blueTeamParticles[i].Play();
+        }
+    }
+}
