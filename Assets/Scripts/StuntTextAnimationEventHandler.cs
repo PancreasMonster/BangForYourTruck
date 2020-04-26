be0b2082620165db25f7b1bef63be03650f08ec6
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-04-26 15:47:38 +0100

    Stunt Text animation event handler set up

---
+++ Assets/Scripts/StuntTextAnimationEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuntTextAnimationEventHandler : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LeaveCrashedState()
+    {
+        anim.SetBool("Crashed",false);
+    }
+
+    public void LeaveGoToScoreTextState()
+    {
+        anim.SetBool("GoToScroText", false);
+    }
+}
