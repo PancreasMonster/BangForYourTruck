76dfad1a34c945a53a767db5d654fd924dd27aea
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-04-27 15:25:55 +0100

    Black hole particles work

--- Assets/Scripts/Throwables Scripts/BlackHole.cs
+++ Assets/Scripts/Throwables Scripts/BlackHole.cs
@@ -16,6 +16,8 @@ public class BlackHole : MonoBehaviour
     public float damage;
     public float explosiveForce;
     MoonGravity gravity;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem vacuumParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +34,14 @@ public class BlackHole : MonoBehaviour
             timeActive += Time.deltaTime;
             range = Mathf.Max(maxRange * ((timeActive / duration)), minRange);
             GetComponent<MoonGravity>().maxDistance = range;
+
+            //particle system gets larger radius over time, at the start it is multiplied by .333, finally buy 1
+            float vacuumParticleRadius;
+            vacuumParticleRadius = Mathf.Max(300 * ((timeActive / duration)), 100);
+            Debug.Log(vacuumParticleRadius);
+            var shape = vacuumParticle.shape;
+            shape.radius = vacuumParticleRadius;
+            //vacuumParticle.shape.radius = vacuumParticleRadius;
         }
     }
 
@@ -61,6 +71,7 @@ public class BlackHole : MonoBehaviour
                 h.TakeDamage("black holed", this.gameObject, damage, Vector3.zero);
         }
         transform.GetChild(0).gameObject.SetActive(false);
+        explosionParticle.Play();
         Invoke("DestroyThisGameObject", 1f);
     }
 
