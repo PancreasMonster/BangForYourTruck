e7dcca7d7d0fcb30859c3d6efbe97c7d524cd301
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-04-30 17:35:11 +0100

    Main menu animations set up, some bugs present

---
+++ Assets/Scripts/MainMenuAnimationEventHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAnimationEventHandler : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToSinglePlayer()
+    {
+        anim.SetBool("SinglePlayerMenu", true);
+    }
+
+    public void GoToMultiPlayer()
+    {
+        anim.SetBool("MultiPlayerMenu", true);
+    }
+
+    public void LeaveSinglePlayer()
+    {
+        anim.SetBool("SinglePlayerMenu", false);
+    }
+
+    public void LeaveMultiPlayer()
+    {
+        anim.SetBool("MultiPlayerMenu", false);
+    }
+}
