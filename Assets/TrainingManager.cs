5ac37bdf64446b478fe951040d362fe7b56d5b84
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-05-03 16:44:54 +0100

    Training work

--- Assets/TrainingManager.cs
+++ Assets/TrainingManager.cs
@@ -5,15 +5,23 @@ using FMODUnity;
 
 public class TrainingManager : MonoBehaviour
 {
+    public Camera camera;
+    public GameObject drone;
     public int trainingStage;
     GameObject trainingCanvas;
+    BoxCollider[] triggers;
 
     public float textDelay;
     AudioSource audio;
+    Animator camAnim;
+    Animator droneAnim;
 
     // Start is called before the first frame update
     void Start()
     {
+        triggers = GetComponentsInChildren<BoxCollider>();
+        camAnim = camera.GetComponent<Animator>();
+        droneAnim = drone.GetComponent<Animator>();
         Training();
         trainingCanvas = GameObject.Find("Training Canvas");
         audio = GetComponent<AudioSource>();
@@ -36,6 +44,9 @@ public class TrainingManager : MonoBehaviour
     {
         if (trainingStage == 0)
         {
+            //show drone coming to the player and talking to them
+            droneAnim.SetTrigger("");
+            camAnim.SetTrigger("");
             Debug.Log(trainingStage);
             textDelay = 2f;
             Invoke("DisplayText", 1f);
@@ -45,14 +56,15 @@ public class TrainingManager : MonoBehaviour
         if (trainingStage == 1)
         {
             Debug.Log(trainingStage);
-
+            droneAnim.SetTrigger("");
+            triggers[0].isTrigger = true;
             trainingCanvas.transform.GetChild(0).gameObject.SetActive(false);
         }
 
         if (trainingStage == 2)
         {
             Debug.Log(trainingStage);
-
+            droneAnim.SetTrigger("");
             textDelay = 3f;
 
         }
@@ -60,21 +72,26 @@ public class TrainingManager : MonoBehaviour
         if (trainingStage == 3)
         {
             Debug.Log(trainingStage);
-
+            droneAnim.SetTrigger("");
             textDelay = 2f;
 
         }
 
         if (trainingStage == 4)
         {
+            //look at hard to reach drone 
             Debug.Log(trainingStage);
-
+            droneAnim.SetTrigger("");
+            camAnim.SetTrigger("");
             textDelay = 3f;
 
         }
 
         if (trainingStage == 5)
         {
+            //watch training drone die, look at killtoken, then look at collection gate 
+            camAnim.SetTrigger("");
+            droneAnim.SetTrigger("");
             Debug.Log(trainingStage);
 
 
@@ -82,7 +99,11 @@ public class TrainingManager : MonoBehaviour
 
         if (trainingStage == 6)
         {
+            //Closeup of drones face fade to black
+            camAnim.SetTrigger("");
+            droneAnim.SetTrigger("");
             Debug.Log(trainingStage);
+            triggers[1].isTrigger = true;
 
 
         }
