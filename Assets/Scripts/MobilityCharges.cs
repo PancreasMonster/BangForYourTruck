5c71c28c4adc01ba485f8e387f7f4db6f79024f2
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-05-04 01:52:27 +0100

    Mobility charges work

--- Assets/Scripts/MobilityCharges.cs
+++ Assets/Scripts/MobilityCharges.cs
@@ -24,20 +24,35 @@ public class MobilityCharges : MonoBehaviour
     public float rechargeTime = 3f;
     public float maxDriftMultiplier;
 
-    SpriteRenderer sprite1;
-    ParticleSystem particles1;
-    SpriteRenderer sprite2;
-    ParticleSystem particles2;
-    SpriteRenderer sprite3;
-    ParticleSystem particles3;
-    public GameObject noChargeParticlesParent;
-    ParticleSystem[] noChargeParticles;
-    public GameObject useChargeParticlesParent;
+    public GameObject emission1;
+    Material emissionMat1;
+    public GameObject emission2;
+    Material emissionMat2;
+    public GameObject emission3;
+    Material emissionMat3;
+
+
+
+    //SpriteRenderer sprite1;
+    //ParticleSystem particles1;
+    //SpriteRenderer sprite2;
+    //ParticleSystem particles2;
+    //SpriteRenderer sprite3;
+    //ParticleSystem particles3;
+    //public GameObject noChargeParticlesParent;
+    //ParticleSystem[] noChargeParticles;
+    //public GameObject useChargeParticlesParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        noChargeParticles = noChargeParticlesParent.GetComponentsInChildren<ParticleSystem>();
+        emissionMat1 = emission1.GetComponent<MeshRenderer>().material;
+
+        emissionMat2 = emission2.GetComponent<MeshRenderer>().material;
+
+        emissionMat3 = emission3.GetComponent<MeshRenderer>().material;
+
+        /*noChargeParticles = noChargeParticlesParent.GetComponentsInChildren<ParticleSystem>();
 
         sprite1 = light1.GetComponent<SpriteRenderer>();
         sprite2 = light2.GetComponent<SpriteRenderer>();    
@@ -46,7 +61,7 @@ public class MobilityCharges : MonoBehaviour
         particles1 = light1.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
         particles2 = light2.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
         particles3 = light3.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
-
+        */
         currentCharges = 3;
     }
 
@@ -72,8 +87,9 @@ public class MobilityCharges : MonoBehaviour
             currentCharges++;
             aud.pitch = .9f;
             aud.Play();
-            particles1.Play();
-            sprite1.color = Color.green;
+            //particles1.Play();
+            //sprite1.color = Color.green;
+            emissionMat1.color = Color.green;
             charge1Time = 0f;
         }
 
@@ -89,8 +105,9 @@ public class MobilityCharges : MonoBehaviour
             currentCharges++;
             aud.pitch = .95f;
             aud.Play();
-            particles2.Play();
-            sprite2.color = Color.green;
+            //particles2.Play();
+            //sprite2.color = Color.green;
+            emissionMat2.color = Color.green;
             charge2Time = 0f;
         }
 
@@ -106,54 +123,64 @@ public class MobilityCharges : MonoBehaviour
             currentCharges++;
             aud.pitch = 1f;
             aud.Play();
-            particles3.Play();
-            sprite3.color = Color.green;
+            //particles3.Play();
+            //sprite3.color = Color.green;
+            emissionMat3.color = Color.green;
             charge3Time = 0f;
         }
 
         if (charge1Time >= rechargeTime/2)
-        {            
-            sprite1.color = Color.yellow;            
+        {
+            //sprite1.color = Color.yellow;
+            emissionMat1.color = Color.yellow;
         }
 
         if (charge2Time >= rechargeTime / 2)
         {
-            sprite2.color = Color.yellow;
+            //sprite2.color = Color.yellow;
+            emissionMat2.color = Color.yellow;
         }
 
         if (charge3Time >= rechargeTime / 2)
         {
-            sprite3.color = Color.yellow;
+            //sprite3.color = Color.yellow;
+            emissionMat3.color = Color.yellow;
         }
 
         if (charge1Time >= (rechargeTime * .25f))
         {
-            sprite1.color = new Color(1.0f, 0.64f, 0.0f);
+            //sprite1.color = new Color(1.0f, 0.64f, 0.0f);
+            emissionMat1.color = new Color(1.0f, 0.64f, 0.0f);
         }
 
         if (charge2Time >= (rechargeTime * .25f))
         {
-            sprite2.color = new Color(1.0f, 0.64f, 0.0f);
+            //sprite2.color = new Color(1.0f, 0.64f, 0.0f);
+            emissionMat2.color = new Color(1.0f, 0.64f, 0.0f);
         }
 
         if (charge3Time >= (rechargeTime * .25f))
         {
-            sprite3.color = new Color(1.0f, 0.64f, 0.0f);
+            //sprite3.color = new Color(1.0f, 0.64f, 0.0f);
+            emissionMat3.color = new Color(1.0f, 0.64f, 0.0f);
         }
 
         if (charge1Time >= (rechargeTime * .75f))
         {
-            sprite1.color = new Color(.64f, 1f, 0.0f);
+            //sprite1.color = new Color(.64f, 1f, 0.0f);
+            emissionMat1.color = new Color(.64f, 1f, 0.0f);
         }
 
         if (charge2Time >= (rechargeTime * .75f))
         {
-            sprite2.color = new Color(.64f, 1f, 0.0f);
+            //sprite2.color = new Color(.64f, 1f, 0.0f);
+            emissionMat2.color = new Color(.64f, 1f, 0.0f);
         }
 
         if (charge3Time >= (rechargeTime * .75f))
         {
-            sprite3.color = new Color(.64f, 1f, 0.0f);
+            //sprite3.color = new Color(.64f, 1f, 0.0f);
+            emissionMat3.color = new Color(.64f, 1f, 0.0f);
         }
     }
 
@@ -162,7 +189,7 @@ public class MobilityCharges : MonoBehaviour
         if (currentCharges == 0)
         {
             Debug.Log("empty");
-            NoChargeTelegraph();
+            //NoChargeTelegraph();
             
         }
         else
@@ -172,7 +199,7 @@ public class MobilityCharges : MonoBehaviour
 
         }
     }
-
+    /*
     void NoChargeTelegraph()
     {
         
@@ -182,31 +209,35 @@ public class MobilityCharges : MonoBehaviour
             noChargeParticlesParent.transform.GetChild(1).GetComponent<ParticleSystem>().Play();
         }
     }
-
+    */
     void ToggleCharges()
     {
         if (charge1 == true)
         {
             charge1 = false;
-            useChargeParticlesParent.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
-            sprite1.color = Color.red;
+            //useChargeParticlesParent.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
+            //sprite1.color = Color.red;
+            emissionMat1.color = Color.red;
             return;
         }
 
         if (charge2 == true)
         {
             charge2 = false;
-            useChargeParticlesParent.transform.GetChild(1).GetComponent<ParticleSystem>().Play();
-            sprite2.color = Color.red;
+            //useChargeParticlesParent.transform.GetChild(1).GetComponent<ParticleSystem>().Play();
+            //sprite2.color = Color.red;
+            emissionMat2.color = Color.red;
             return;
         }
 
         if (charge3 == true)
         {
             charge3 = false;
-            useChargeParticlesParent.transform.GetChild(2).GetComponent<ParticleSystem>().Play();
-            sprite3.color = Color.red;
+            //useChargeParticlesParent.transform.GetChild(2).GetComponent<ParticleSystem>().Play();
+            //sprite3.color = Color.red;
+            emissionMat3.color = Color.red;
             return;
         }
     }
+    
 }
