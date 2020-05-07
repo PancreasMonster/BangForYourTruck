043f566770127bf5bcfb2921a5838d0e05ab3dfe
Nicolas_St <nicholas.stevenson@pulsecollegestudent.com>
2020-05-07 17:12:16 +0100

    Animation Event Handler

---
+++ Assets/TrainingAnimationEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingAnimationEventHandler : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Camera droneCamera;
+
+
+    public void SwapToDroneCamera()
+    {
+        playerCamera.enabled = false;
+        droneCamera.enabled = true;
+    }
+
+    public void SwapToPlayerCamera()
+    {
+        droneCamera.enabled = false;
+        playerCamera.enabled = true;
+    }
+}
