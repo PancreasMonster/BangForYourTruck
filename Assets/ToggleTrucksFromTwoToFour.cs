9829e09fb6f62dba87b771dfbbe606af70db6e70
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2020-05-11 20:48:57 +0100

    Main Menu animations work

---
+++ Assets/ToggleTrucksFromTwoToFour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ToggleTrucksFromTwoToFour : MonoBehaviour
+{
+    bool twoTrucks;
+
+    public void ToggleTrucks() 
+    {
+        if (twoTrucks)
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+            transform.GetChild(3).gameObject.SetActive(true);
+            twoTrucks = false;
+        }
+        else 
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
+            transform.GetChild(3).gameObject.SetActive(false);
+            twoTrucks = true;
+        }
+    }
+}
