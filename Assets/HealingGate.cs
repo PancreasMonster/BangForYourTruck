d21b735169e1ce690b7609447c1c5f63477d514f
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2020-05-12 19:08:41 +0100

    Healing gate fix

---
+++ Assets/HealingGate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingGate : MonoBehaviour
+{
+    public int price;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.tag == "Player")
+        {
+
+            PlayerBank PB = col.GetComponent<PlayerBank>();
+            if (PB.tagsInBank >= price)
+            {
+                if (col.GetComponent<Health>().health < col.GetComponent<Health>().maxHealth)
+                {
+                    col.GetComponent<Health>().health = 100;
+                    PB.tagsInBank -= price;
+                    audio.Play();
+                }
+
+            }
+
+        }
+    }
+}
