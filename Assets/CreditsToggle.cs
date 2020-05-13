0b6a4f42e8d9d19d6caf9f64e1b3c39f1495ecc6
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2020-05-13 22:02:53 +0100

    Update CreditsToggle.cs, for real this time...

--- Assets/CreditsToggle.cs
+++ Assets/CreditsToggle.cs
@@ -24,8 +24,8 @@ public class CreditsToggle : MonoBehaviour
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(1).gameObject.SetActive(true);
             transform.GetChild(2).gameObject.SetActive(true);
-            transform.GetChild(3).gameObject.SetActive(false);
-            transform.GetChild(4).gameObject.SetActive(false);
+            transform.GetChild(3).gameObject.SetActive(true);
+            transform.GetChild(4).gameObject.SetActive(true);
             credits = true;
         }
     }
