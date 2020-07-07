f5d3eb19930ef4201706158e35e706c2da0e2134
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2020-07-07 16:24:20 +0100

    Shotgun bullet fix
    no longer can shoot through walls

---
+++ Assets/ShotgunBulletDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ShotgunBulletDeath : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col) 
+    {
+        if (col.gameObject.layer == 14) 
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
