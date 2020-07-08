f6a39079be4fad27ea0abd0fc65d84e0c6d141a9
PancreasMonster <ryanflood444@gmail.com>
2020-07-08 16:21:27 +0100

    Players Joining UI

--- Assets/Scripts/PlayersJoining/PlayerConnectItem.cs
+++ Assets/Scripts/PlayersJoining/PlayerConnectItem.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerConnectItem : MonoBehaviour
 {
+    public bool player1;
+
+    public string connectState;
+
+    public string connectName;
+
+    private void OnDeviceLost(InputValue value)
+    {
+        if (!player1)
+            connectState = "Press X to Join";
+    }
+
+    private void OnFaceButtonWest(InputValue value)
+    {
+        if (!player1)
+            connectState = connectName;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (player1)
+            connectState = "Player 1";
     }
 
     // Update is called once per frame
