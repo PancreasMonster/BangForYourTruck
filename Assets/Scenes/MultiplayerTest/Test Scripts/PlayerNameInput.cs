4add4d4964b1542f32d1a835c4c4884e66bac568
PancreasMonster <ryanflood444@gmail.com>
2020-07-17 12:44:51 +0100

    Photon Changes

---
+++ Assets/Scenes/MultiplayerTest/Test Scripts/PlayerNameInput.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using Photon.Pun;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField nameInputField = null;
+
+    [SerializeField]
+    private Button continueButton = null;
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+
+    private void Start()
+    {
+        SetUpInputField();
+    }
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+        nameInputField.text = defaultName;
+
+        InitialisePlayerName(defaultName);
+    }
+
+    public void InitialisePlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SetPlayerName()
+    {
+        string name = nameInputField.text;
+
+        if (name == "")
+            name = null;
+
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        string playerName = nameInputField.text;
+
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+    }
+}
